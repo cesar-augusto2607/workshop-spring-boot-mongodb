f10901d03cae386ac1e546308c7754c41f1ad2e7
Cesar Augusto <cesaraugustoc@hotmail.com>
2025-04-02 10:59:37 -0300

    obtendo usuario por id

--- src/main/java/com/cesaraugusto/workshop/mongo/service/UserService.java
+++ src/main/java/com/cesaraugusto/workshop/mongo/service/UserService.java
@@ -1,12 +1,14 @@
 package com.cesaraugusto.workshop.mongo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.cesaraugusto.workshop.mongo.domain.User;
 import com.cesaraugusto.workshop.mongo.repository.UserRepository;
+import com.cesaraugusto.workshop.mongo.service.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
---
+++ src/main/java/com/cesaraugusto/workshop/mongo/service/exception/ObjectNotFoundException.java
@@ -0,0 +1,10 @@
+package com.cesaraugusto.workshop.mongo.service.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+}
