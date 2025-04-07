e755d6d461e0ea726030388a74ceddec39f77ff1
Cesar Augusto <cesaraugustoc@hotmail.com>
2025-04-07 09:52:54 -0300

    Obtendo post por id

---
+++ src/main/java/com/cesaraugusto/workshop/mongo/resources/PostResource.java
@@ -0,0 +1,27 @@
+package com.cesaraugusto.workshop.mongo.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cesaraugusto.workshop.mongo.domain.Post;
+import com.cesaraugusto.workshop.mongo.service.PostService;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+	@Autowired
+	private PostService service;
+
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Post> findById(@PathVariable String id) {
+		Post obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+
+}
